f666988694881031de3826fe3e1bc70aa969f2e7
Sarvar <103188744+Sarvar2004@users.noreply.github.com>
2022-08-03 20:55:27 +0500

    Элементы, равные максимуму
    Решение задачи Элементы, равные максимуму acmp.ru

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int m,k=1,x;
+    scanf("%d",&m);
+	do
+		{
+		scanf("%d",&x);
+		if (x>m){
+		    m=x;k=1;
+		}
+		else if(x==m){
+		    k++;
+		}
+	}while(x);
+	printf("%d",k);
+
+
+    return 0;
+}
